dc72be0cd53bd447e6484c252cc642ae620cb7bb
SHUBHAM THAKUR <shubhamwert@gmail.com>
2019-04-11 22:55:55 +0530

    added chat room \n UI improvements for chat room

--- app/src/main/java/com/frictionhacks/chatbox/Activity/LoginActivity.java
+++ app/src/main/java/com/frictionhacks/chatbox/Activity/LoginActivity.java
@@ -1,16 +1,12 @@
 package com.frictionhacks.chatbox.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 
-import com.frictionhacks.chatbox.Fragment.SignInFragment;
 import com.frictionhacks.chatbox.Fragment.SignUpFragment;
 import com.frictionhacks.chatbox.R;
 
@@ -35,22 +31,12 @@ boolean b=false;
         change();
     }
     public void change() {
-      if (b){ SignUpFragment signUpFragment=new SignUpFragment();
+     SignUpFragment signUpFragment=new SignUpFragment();
         FragmentManager l=getSupportFragmentManager();
         FragmentTransaction ft=l.beginTransaction();
 
         ft.replace(R.id.fl_login_sign_options,signUpFragment);
         ft.commit();
-      b=false;}
-        else{
-          SignInFragment signInFragment=new SignInFragment();
-          FragmentManager l=getSupportFragmentManager();
-          FragmentTransaction ft=l.beginTransaction();
-
-          ft.replace(R.id.fl_login_sign_options,signInFragment);
-          ft.commit();
-          b=true;
-
-      }
+      b=false;
     }
 }
